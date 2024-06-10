6e1c637e5c928067eb0af598e95d77c5f8459de7
Carlos Camilo <carloscamilocontato@gmail.com>
2024-06-10 17:21:22 -0300

    substitute lazyvim to nvchad

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -3,9 +3,6 @@ sudo apt update
 sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
-
-	# Enable default extras
-	cp ~/.local/share/omakub/configs/lazyvim.json ~/.config/nvim/lazyvim.json
+  git clone https://github.com/NvChad/starter ~/.config/nvim
+  rm -rf ~/.config/nvim/.git
 fi
